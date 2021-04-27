aff34a09bcb21200ec2465e7e92c006e205ca6d7
squatcub <brandon.alexis10@hotmail.com>
2021-04-27 00:22:05 -0500

    Avance previo a la implementacion de la API

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // Barra de busqueda
+    @IBOutlet weak var ciudadTextField: UITextField!
+    
+    // Elementos modificables
+    @IBOutlet weak var climaImageView: UIImageView!
+    @IBOutlet weak var temperaturaLabel: UILabel!
+    @IBOutlet weak var ciudadLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        // Do any additional setup after loading the view.
+        
+        ciudadTextField.delegate = self
     }
-
-
+    // Funcion para localizacion
+    @IBAction func gpsButton(_ sender: Any) {
+    }
+    // Funcion para buscar ciudad
+    @IBAction func buscarButton(_ sender: Any) {
+        //Ocultar teclado
+        ciudadTextField.endEditing(true)
+        //ciudadLabel.text = ciudadTextField.text
+        //ciudadTextField.text = ""
+    }
+    
+    
+    // Funcion del delegado, cuando se presiona el boton del teclado
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Ocultar teclado
+        ciudadTextField.endEditing(true)
+        // ciudadLabel.text = ciudadTextField.text
+        //ciudadTextField.text = ""
+        return true
+    }
+    
+    // No avanzar si no hay texto. Esconder el teclado
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if ciudadTextField.text != "" {
+            return true
+        } else {
+            ciudadTextField.placeholder = "Ingresa el nombre de una ciudad"
+            return false
+        }
+    }
+    
+    // Ya se termino de ingresar texto
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        ciudadLabel.text = ciudadTextField.text
+        ciudadTextField.text = ""
+    }
+    
 }
 
